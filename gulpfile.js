5ac976afbdb7e9f1452c4c3892f1b7fbae74093b
k27chang <kearychang@hotmail.com>
2024-07-06 11:37:45 -0400

    moved folders to folders-wedding

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,25 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 // compile scss to css
-gulp.task('sass', function () {
-    return gulp.src('./sass/styles.scss')
+gulp.task('sass-wedding', function () {
+    return gulp.src('./sass-wedding/styles.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({basename: 'styles.min'}))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css-wedding'));
 });
 
 // watch changes in scss files and run sass task
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('sass-wedding:watch', function () {
+    gulp.watch('./sass-wedding/**/*.scss', ['sass-wedding']);
 });
 
 // minify js
 gulp.task('minify-js', function () {
-    return gulp.src('./js/scripts.js')
+    return gulp.src('./js-wedding/scripts.js')
         .pipe(uglify())
         .pipe(rename({basename: 'scripts.min'}))
-        .pipe(gulp.dest('./js'));
+        .pipe(gulp.dest('./js-wedding'));
 });
 
 // default task
-gulp.task('default', gulp.series('sass', 'minify-js'));
+gulp.task('default', gulp.series('sass-wedding', 'minify-js'));
